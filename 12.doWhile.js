a9e1a82411f4b1a69874f728801616e679036f3b
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-29 00:14:00 +0600

    Create 12.doWhile.js

---
+++ 12.doWhile.js
@@ -0,0 +1,16 @@
+// do while print 0-10
+// var i= 0;
+// do{   
+//     console.log(i);  
+//     i++;
+// }while(i<10);
+
+// infinite loop with print 1-10 using break
+var i = 0;
+do{
+    i++;
+    console.log(i);
+    if(10==i){
+        break;
+    }
+}while(true);
