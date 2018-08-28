29d0d3e49d4addadda67ae4cef5df19bd66bd6a0
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-29 00:15:42 +0600

    Create 15.factorial.js

---
+++ 15.factorial.js
@@ -0,0 +1,25 @@
+// give me factorial of number 5 using for loop
+// var n = 10;
+// var factorial =1;
+// for(var i=n;i>1;i--){
+//     factorial*=i;
+// }
+// console.log(factorial);
+
+
+//factorial print 1-10 number using for
+
+// for(var j=0;j<=10;j++){
+//      factorial =1;
+//     for(var i=j;i>0;i--){
+//         factorial*=i;
+//     }
+//     console.log('factorial of',j,'is',factorial);
+// }
+
+// factorial print 1-10 use simple way
+var factorial =1;
+for(var i=1;i<=10;i++){
+    factorial*=i;
+    console.log('factorial of',i,'is',factorial);
+}
