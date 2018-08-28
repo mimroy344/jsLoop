028329b686f911b125929caf03ada47756067c69
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-28 13:32:39 +0600

    Create 9.while.js

---
+++ 9.while.js
@@ -0,0 +1,33 @@
+/****** print 1-10 *******/
+// print 1-10 by condition i++
+var i = 0;
+while(i++<10){
+    console.log(i);
+}
+// print 1-10 by condition ++i
+var i = 0;
+while(++i<10){
+    console.log(i);
+}
+
+
+/**** print 0-10 ****/
+// print 0-10 by condition after i++
+var i= 0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+/***** print 9-0 *****/
+// print 0-9
+var i = 10
+while(i>0){
+    i--;
+    console.log(i);
+}
+// print 9-0
+var i = 10;
+while(i-->0){
+    console.log(i);
+}
