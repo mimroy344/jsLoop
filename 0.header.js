7be71d4c484f832978367d712eb9804c23113c53
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-28 13:31:57 +0600

    Create 0.header.js

---
+++ 0.header.js
@@ -0,0 +1,12 @@
+/*9.while Loop*/
+1.print 1-10
+2.print 0-10
+3.print 9-0
+
+/*10.while Loop*/
+1.infinite Loop
+2.print 1-10
+3.print even number between 0-10
+4.print even number between 0-10 by simple way
+5.print 0-10 with gap 3
+6.print 0-10 by infinite loop
