ba3ed6ede6fbf1167250fb88ce2eae69323fcfae
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-29 00:06:40 +0600

    Update 0.header.js

--- 0.header.js
+++ 0.header.js
@@ -1,12 +1,65 @@
-/*9.while Loop*/
+//1.math
+1.cost of eggs --calculator
+2.area of circle --calculator
+3.usd to bdt --calculator
+4.remainder --show
+
+//2.increment & compound assignment operator
+1.pre & post increment --compound assignment operaotr
+
+//3.decrement & compound assignment operator
+1.pre & post decrement
+
+//4.condition
+1.comparison operator --smaller or greater
+2.if conditional statement --jamal&Kamal nesting
+3.if multiple statement check--they are brother
+
+//5.nesting
+1.if nesting  --prime number
+2.positive & negative number
+3.even & odd 
+
+//6.movie
+1.movie Rating --calculate
+
+//7.age -calculate
+1.your age to condition--adult ,teenager,baby
+
+//8.ternary
+1.ternary operator --even,positive
+
+// 9.while
 1.print 1-10
 2.print 0-10
 3.print 9-0
 
-/*10.while Loop*/
+//10.while Loop
 1.infinite Loop
 2.print 1-10
 3.print even number between 0-10
 4.print even number between 0-10 by simple way
 5.print 0-10 with gap 3
-6.print 0-10 by infinite loop
+6.print 0-10 by infinite 
+
+//11.deferent i++
+1.print 1-5 by i++ & ++i
+
+//12.doWhile
+1.print 1-10
+2.print  1-10 with 2 gap
+3.infinite loop with print 1-10 using break
+
+//13.forLoop
+1.print 1-10
+2.print 1-10 between even number
+3.print 1-10 between odd number
+4.print 1-10 by infinite loop
+
+//14.multiple statement using for
+1.print 1-10 & 11-20 using for
+
+//15.factorial using for
+1.give me number of 5 factorial
+2.factorial print 1-10 using for
+3.factorial print 1-10 by simple way
