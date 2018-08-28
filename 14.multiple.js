edd467d70d3b289a5c8f41d67532b6822cc47b54
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-29 00:15:11 +0600

    Create 14.multiple.js

---
+++ 14.multiple.js
@@ -0,0 +1,13 @@
+// multiple statement using for
+// var i,j;
+// for (i=0,j=10;i<=10;i++,j--){
+//     console.log(i,j);
+// }
+
+
+
+// multiple statement
+var i,j;
+for (i=1,j=11;i<=10;i++,j++){
+    console.log(i,j);
+}
