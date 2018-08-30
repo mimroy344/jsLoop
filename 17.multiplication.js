89b3b7f4289cec9b2b65677fdba37167ad0793e2
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:33:47 +0600

    Create 17.multiplication.js

---
+++ 17.multiplication.js
@@ -0,0 +1,19 @@
+// multiplication table 1-10
+// var series;
+// for(var i=1;i<=10;i++){
+//     series=i+'=';
+//     for(var j=1;j<=10;j++){
+//         series=series+i*j+' ';
+//     }
+// console.log(series);
+// }
+
+// multiplication table 1-10
+var series;
+for(i=1;i<=10;i++){
+    series=i+'=';
+    for(var j=1;j<=10;j++){
+        series=series+i*j +' ';
+    }
+    console.log(series)
+}
