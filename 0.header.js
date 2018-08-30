ab9875981d7b651dbf32954a0b52821fe5bb5926
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:24:00 +0600

    Update 0.header.js

--- 0.header.js
+++ 0.header.js
@@ -3,29 +3,22 @@
 2.area of circle --calculator
 3.usd to bdt --calculator
 4.remainder --show
-
 //2.increment & compound assignment operator
 1.pre & post increment --compound assignment operaotr
-
 //3.decrement & compound assignment operator
 1.pre & post decrement
-
 //4.condition
 1.comparison operator --smaller or greater
 2.if conditional statement --jamal&Kamal nesting
 3.if multiple statement check--they are brother
-
 //5.nesting
 1.if nesting  --prime number
 2.positive & negative number
 3.even & odd 
-
 //6.movie
 1.movie Rating --calculate
-
 //7.age -calculate
 1.your age to condition--adult ,teenager,baby
-
 //8.ternary
 1.ternary operator --even,positive
 
@@ -43,7 +36,7 @@
 6.print 0-10 by infinite 
 
 //11.deferent i++
-1.print 1-5 by i++ & ++i
+1.print 1-5 by i++ & ++i using while loop
 
 //12.doWhile
 1.print 1-10
@@ -63,3 +56,89 @@
 1.give me number of 5 factorial
 2.factorial print 1-10 using for
 3.factorial print 1-10 by simple way
+
+// 16.number series
+0.// series0= 1 2 3 4 5 6 7 8 9
+1.// s1= 2 4 6 8 10 12 14 ....
+2.// s2= 1 4 7 10 13 16 .....
+3.// s3= 0 3 8 15 24 35 48 ....
+4.// s4 =1 4 3 8 5 12 7 16 9 20 ...
+5.// s5 =0 1 1 2 3 5 8 13 ....fibonacci
+
+// 17.multiplication table
+1.multiplication table 1-10 using for
+
+// 18.divisors
+1.divisor any number 
+
+// 19.Calendar
+1.print Calendar of April
+
+// 20.array
+1.array
+
+// 21.intro array
+1.arr er jkno akta Element print,
+2.arr te a Element add,
+3.arr length,
+4.ses element access (lastOffset)
+
+// 22.array manipulation
+0.arr [arr.length]='element';
+1.push
+2.pop
+3.shift
+4.unshift
+
+// 23.delete
+1.delete method
+
+// 24.slice
+1.slice method with positive index
+2.slice method with negative index
+3.last 3 ta element access without any method
+
+// 25.splice
+1.splice method with positive index
+2.splice method with negative index
+3.last 3 ta element access without any method
+
+// 26.clone&copy
+1.shallow copy
+2.deep copy 
+3.deep copy solution with slice 
+4.deep copy solution with [...arr]
+5.deep copy solution with Array.from(arr)//arr is (arr name)
+
+// 27.merge(addition)
+1.sequence concate 
+2.unsequence concate 
+3.multiple way
+4.arr.concate(arr,arr)
+
+// 28.traverse
+1.traverse
+2.traverse whthout extra processing
+3.traverse with in 
+4.reverse 
+
+// 29.empty
+1.arr theke empty element bad 
+2.arr theke empty element bad with filter method
+
+// 30.sort
+1.sort method
+2.variable
+3.new swap way
+4.sort method with for loop 
+
+// 31.string-array
+1.array to string 
+2.string to array 
+3.simple way to split and join 
+4.reverse
+
+// 32.how many
+1.how many word in a sentense 
+2.how many character length in a sentense 
+3.how many alphabet in a sentense 
